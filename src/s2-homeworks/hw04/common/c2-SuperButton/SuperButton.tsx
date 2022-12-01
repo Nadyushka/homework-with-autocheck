914b94e8cad8cadd9406faab90d23eda2650aabd
Nadyushka <12nadya11@gmail.com>
2022-12-01 22:25:42 +0300

    fix mistakes and pass all tests HW4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -20,7 +20,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     const finalClassName = `
     ${s.button} 
     ${disabled ? s.disabled : xType === 'red' ? s.red : xType === 'secondary' ? s.secondary : s.default} 
-    ${className ? s.className : ''}`
+    ${className && s.className}`
     // + (disabled
     //         ? ...
     //         : xType === 'red'
