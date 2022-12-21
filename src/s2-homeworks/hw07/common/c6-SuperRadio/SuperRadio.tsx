c587c53ab2d047a1d8a9e6a847c555fab99761d8
Nadyushka <12nadya11@gmail.com>
2022-12-21 21:27:43 +0300

    hw7 onChangeCallback SuperRadio add

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -36,6 +36,8 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // делают студенты
+        console.log(e.currentTarget.name)
+        onChangeOption && onChangeOption(e.currentTarget.value)
     }
 
     const finalRadioClassName = s.radio + (className ? ' ' + className : '')
@@ -49,6 +51,9 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       className={finalRadioClassName}
                       type={'radio'}
                       // name, checked, value делают студенты
+                      name = {o.value}
+                      checked = { value === o.id}
+                      value={o.id}
 
                       onChange={onChangeCallback}
                       {...restProps}
