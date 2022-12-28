c478a59162ebd242c2dfd8c69d6f13c18c3af506
Nadyushka <12nadya11@gmail.com>
2022-12-28 11:06:12 +0300

    hw8 add homeWorkReducer sort

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,14 +7,13 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-       //     action.payload === 'up' ? state.sort((a, b) => a.name.localeCompare(b.name)) :
-       //         state.sort((a, b) => b.name.localeCompare(a.name))
+          action.payload === 'up' ? state.sort((a, b) => a.name.localeCompare(b.name)) :
+               state.sort((a, b) => b.name.localeCompare(a.name))
             return [...state]
         }
         case 'check': {
 
-            return state // need to fix
-        }
+            return state /* need to fix */}
         default:
             return state
     }
